a4f7250948d09043d0f698f695cd28eb6f648770
khanian <khanylee@gmail.com>
2023-10-12 20:10:42 +0900

    test icon and button

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,23 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(onPressed: () {
+            print('Tab!');
+          }, icon: Icon(Icons.home)),
+          Icon(Icons.play_arrow),
+        ],
+        centerTitle: false,
+        title: Text('This is App bar'),
+      ),
       body: TestWidget(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.bug_report),
+        onPressed: () {
+          print('Tab! FAB!');
+        },
+      ),
     ),
   ));
 }
